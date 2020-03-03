551a0d4d7a34f36d2bbcfb1e85c438c52d03ff71
Matthew Ritter <mritter4@villanova.edu>
2020-03-02 20:57:27 -0600

    Added Ball Power Meter

---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public int maxPower;
+    public int minPower;
+    public int power;
+
+    private Rigidbody golfBall;
+
+    public bool isMoving;
+
+    private bool increasing;
+    private bool powerChanging;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        golfBall = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!(isMoving))
+        {
+            if ((Input.GetKeyDown("space")) && !(powerChanging))
+            {
+                powerChanging = true;
+                StartCoroutine(changePower());
+            }
+            else if ((Input.GetKeyDown("space")) && (powerChanging))
+            {
+                powerChanging = false;
+            }
+
+        }
+    }
+
+    private void StartCoRoutine(IEnumerator enumerator)
+    {
+        throw new NotImplementedException();
+    }
+
+    private IEnumerator changePower()
+    {
+        while (powerChanging)
+        {
+            if (increasing)
+            {
+                if (power + 1 > maxPower)
+                {
+                    power--;
+                    increasing = false;
+                }
+                else
+                {
+                    power++;
+                }
+            }
+            else
+            {
+                if (power - 1 < minPower)
+                {
+                    power++;
+                    increasing = true;
+                }
+                else
+                {
+                    power--;
+                }
+            }
+            yield return new WaitForSeconds(.05f);
+        }
+        powerChanging = false;
+    }
+
+
+
+
+}
