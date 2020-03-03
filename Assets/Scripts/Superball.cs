cc3d86e495881e3853dd5dcd0a09d669fa99d241
Trey Roche <proche@luc.edu>
2020-03-02 21:39:36 -0600

    Superbois

---
+++ Assets/Scripts/Superball.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Superball : MonoBehaviour
+{
+    public Vector3 hit(Vector3 force, int speed){
+        force.Normalize();
+        force *= (10 * speed);
+        return force;
+    }
+}
