fda8c76093832d0f24c7a9232db3b1ce840254f5
Trey Roche <proche@luc.edu>
2020-03-02 20:29:15 -0600

    Add Club class file

---
+++ Assets/Scripts/Club.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Club : MonoBehaviour
+{
+    public Vector3 hit(Vector3 force, int speed){
+        force.Normalize();
+        force *= speed;
+        return force;
+    }
+}
