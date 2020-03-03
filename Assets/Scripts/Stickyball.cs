998d59fa6e4a4f4a728b8f0a2a6bacd0ce7b6372
Trey Roche <proche@luc.edu>
2020-03-03 13:53:07 -0600

    * Dropball.cs: Add reset method
    * Stickyball.cs: Add reset method

--- Assets/Scripts/Stickyball.cs
+++ Assets/Scripts/Stickyball.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Stickyball : MonoBehaviour
 {
-    public void Stick(GameObject ball){
+    public void Stick(GameObject ball)
+    {
         Rigidbody golfBall = ball.GetComponent<Rigidbody>();
         golfBall.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+    }
+
+    public void Reset(GameObject ball)
+    {
+        Rigidbody golfBall = ball.GetComponent<Rigidbody>();
+        golfBall.constraints = RigidbodyConstraints.None;
     }
 }
