e8b437a4a38ac655da1070e8542278440fccbb02
Matthew Ritter <mritter4@villanova.edu>
2020-03-08 00:44:33 -0500

    IDK

--- Assets/Scripts/hole.cs
+++ Assets/Scripts/hole.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class hole : MonoBehaviour
 {
 
-    public float radius = 2;
+    public float radius = 1;
+    private GameObject parent;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        parent = gameObject.transform.parent.gameObject;
     }
 
     // Update is called once per frame
@@ -19,4 +20,23 @@ public class hole : MonoBehaviour
     {
         
     }
+
+    public void changeRadius(float newRadius)
+    {
+        radius = newRadius;
+        Vector3 newScale = new Vector3(radius, radius, radius);
+        gameObject.transform.localScale = newScale;
+        parent.transform.localScale = newScale;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("GolfBall"))
+        {
+            print("You made it!");
+            //Script to move to the next hole
+        }
+    }
+
+
 }
